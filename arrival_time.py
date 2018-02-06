89f92fda21ed79be4f0c4e14a9707784d781a8ee
sppitts47794 <sppdevelopment47794@gmail.com>
2018-02-06 16:20:07 -0500

    Added commute to mom and dads house

--- arrival_time.py
+++ arrival_time.py
@@ -3,13 +3,28 @@ import json
 import requests
 import pprint
 
+# 87+Marlborough+Street+Boston,MA
+# 1+Charles+Street+Rockland,MA
+
 def rockland_to_quincy():
     # Google directions API url endpoint shortened
-    endpoint_url = "https://tinyurl.com/ydepge9c"
+    endpoint_url = "https://maps.googleapis.com/maps/api/directions/json?origin=&destination=1250+Hancock+Street+Quincy,MA&key=AIzaSyBA9_TNdq1Sg9idLBsSvGi30pQqm0YfSZc"
 
     eta_request = urllib.urlopen(endpoint_url)
     eta_response = json.load(eta_request)
 
-    final_eta = eta_response['routes'][0]['legs'][0]['duration_in_traffic']['text']
+    final_eta = eta_response['routes'][0]['legs'][0]['duration']['text']
+
+    return final_eta
+
+
+def rockland_to_boston():
+
+    endpoint_url = "https://maps.googleapis.com/maps/api/directions/json?origin=&destination=&key=AIzaSyBA9_TNdq1Sg9idLBsSvGi30pQqm0YfSZc"
+
+    eta_request = urllib.urlopen(endpoint_url)
+    eta_response = json.load(eta_request)
+
+    final_eta = eta_response['routes'][0]['legs'][0]['duration']['text']
 
     return final_eta
