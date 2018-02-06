2a073bb4d346443737c6448859a7a40e0d5b664a
sppitts47794 <sppdevelopment47794@gmail.com>
2018-02-06 16:21:01 -0500

    Added Bitcoin to list of cryptocurrencys

--- crypto_script.py
+++ crypto_script.py
@@ -2,6 +2,21 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
+
+def get_bitcoin_price_from():
+
+    bitcoin_url = 'https://coinmarketcap.com/currencies/bitcoin/'
+
+    page = urllib2.urlopen(bitcoin_url).read()
+
+    soup = BeautifulSoup(page,'lxml')
+
+    bc_price = soup.find_all('span',class_='text-large2')
+
+    for value in bc_price:
+        return "${}".format(value.text)
+
+
 def get_litecoin_price_from():
 
     litecoin_url = 'https://coinmarketcap.com/currencies/litecoin/'
@@ -12,8 +27,10 @@ def get_litecoin_price_from():
 
     litecoin_price = soup.find_all('span',class_='text-large2')
 
-    for a in litecoin_price:
-         return "${}".format(a.text)
+    for value in litecoin_price:
+         return "${}".format(value.text)
+
+
 
 def get_xr_price_from():
 
@@ -25,5 +42,5 @@ def get_xr_price_from():
 
     ripple_price = soup.find_all('span', class_='text-large2')
 
-    for xr in ripple_price:
-        return "${}".format(xr.text)
+    for value in ripple_price:
+        return "${}".format(value.text)
